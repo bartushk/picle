ad7416a94360ccf305d7ff36ef035dbfd1a2462e
Kyle Bartush <bartushk@gmail.com>
2016-04-12 23:14:44 -0400

    Added a gradle application plugin and the opencv librairies.

---
+++ src/main/java/net/bartushk/picle/Program.java
@@ -0,0 +1,13 @@
+package net.bartushk.picle;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Scalar;
+
+public class Program
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/test/java/net/bartushk/picletest/OpencvTest.java
@@ -0,0 +1,21 @@
+package net.bartushk.picle.test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Scalar;
+
+
+public class OpencvTest
+{
+    @Test
+    public void OpencvBasicTest(){
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        byte buff[] = new byte[1];
+        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(4));
+        m.get(0, 0, buff);
+        assertEquals( buff[0], 4);
+    }
+}
