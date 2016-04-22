cdc800fb286fa2cd96493a398606fb52d0c37b8a
Kyle Bartush <bartushk@gmail.com>
2016-04-21 22:55:10 -0400

    Added a base class for testing filters and test cases for color filter apply function.

---
+++ src/test/java/net/bartushk/picletest/BaseFilterTest.java
@@ -0,0 +1,21 @@
+package net.bartushk.picletest;
+
+import org.junit.BeforeClass;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import net.bartushk.picle.Program;
+
+public class BaseFilterTest
+{
+    public Mat catImage;
+
+    public BaseFilterTest(){
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        String path = Program.class.getResource("/images/cat1.jpeg").getPath();
+        catImage = Imgcodecs.imread(path);
+    }
+
+}
--- src/test/java/net/bartushk/picletest/ColorFilterTest.java
+++ src/test/java/net/bartushk/picletest/ColorFilterTest.java
@@ -1,17 +1,28 @@
 package net.bartushk.picletest;
 
-import net.bartushk.picle.Filter.*;
-import static net.bartushk.picletest.TestUtils.EPSILON;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
 import org.opencv.core.Core;
-import org.opencv.core.Mat;
 import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import net.bartushk.picle.Filter.*;
+import net.bartushk.picle.Program;
+import static net.bartushk.picletest.TestUtils.EPSILON;
+import static org.junit.Assert.*;
 
 
-public class ColorFilterTest
+public class ColorFilterTest extends BaseFilterTest
 {
+    public ColorFilterTest(){
+        super();
+    }
 
     @Test
     public void ColorFilter_WhenConstructed_RedPropertyIntialized(){
@@ -44,6 +55,57 @@ public class ColorFilterTest
         assertEquals(prop.getMin(), 0.0, EPSILON);
         assertEquals(prop.getMax(), 1.0, EPSILON);
         assertEquals(prop.getStep(), 0.01, EPSILON);
+    }
+
+    @Test
+    public void ColorFilter_ApplyFilter_RedPropertyWorking(){
+        ColorFilter filter = new ColorFilter();
+        filter.setProperty("Red", 0);
+
+        Mat transformed = filter.applyFilter(catImage);
+        Scalar elements = Core.sumElems(transformed);
+
+        double blueSum = elements.val[0];
+        double greenSum = elements.val[1];
+        double redSum = elements.val[2];
+
+        assertNotEquals(blueSum, 0, EPSILON);
+        assertNotEquals(greenSum, 0, EPSILON);
+        assertEquals(redSum, 0, EPSILON);
+    }
+
+    @Test
+    public void ColorFilter_ApplyFilter_BluePropertyWorking(){
+        ColorFilter filter = new ColorFilter();
+        filter.setProperty("Blue", 0);
+
+        Mat transformed = filter.applyFilter(catImage);
+        Scalar elements = Core.sumElems(transformed);
+
+        double blueSum = elements.val[0];
+        double greenSum = elements.val[1];
+        double redSum = elements.val[2];
+
+        assertEquals(blueSum, 0, EPSILON);
+        assertNotEquals(greenSum, 0, EPSILON);
+        assertNotEquals(redSum, 0, EPSILON);
+    }
+
+    @Test
+    public void ColorFilter_ApplyFilter_GreenPropertyWorking(){
+        ColorFilter filter = new ColorFilter();
+        filter.setProperty("Green", 0);
+
+        Mat transformed = filter.applyFilter(catImage);
+        Scalar elements = Core.sumElems(transformed);
+
+        double blueSum = elements.val[0];
+        double greenSum = elements.val[1];
+        double redSum = elements.val[2];
+
+        assertNotEquals(blueSum, 0, EPSILON);
+        assertEquals(greenSum, 0, EPSILON);
+        assertNotEquals(redSum, 0, EPSILON);
     }
 
 }
